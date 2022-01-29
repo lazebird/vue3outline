618f3ca6f17169defacc75a8982b654be5b487ad
lazebird <lazebird@gmail.com>
2022-01-29 14:51:31 +0800

    fix: more

--- src/views/HelloWorld.vue
+++ src/views/HelloWorld.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="example">
     <div class="nav">
-      <h1> 目录 </h1>
-      <outline-tree :tree-data="navTree">
-        <template #default="{ scope }">
-          <span class="content_node" @click.stop="jumpToAnchor(scope.row.el)"> {{ scope.row.title }} </span>
-        </template>
-      </outline-tree>
+      <el-affix target=".nav">
+        <h1> 目录 </h1>
+        <outline-tree :tree-data="navTree">
+          <template #default="{ scope }">
+            <span class="content_node" @click.stop="jumpToAnchor(scope.row.el)"> {{ scope.row.title }} </span>
+          </template>
+        </outline-tree>
+      </el-affix>
     </div>
     <div v-outline="tocProps" class="content"> <TestArticle /> </div>
   </div>
