65c35807aa54db831aad5b98348a483a50403cb8
lazebird@163.com <>
2022-02-07 17:09:57 +0800

    fix: more

--- vite.config.ts
+++ vite.config.ts
@@ -6,25 +6,31 @@ function pathResolve(dir: string) {
   return resolve(process.cwd(), '.', dir);
 }
 
-export default defineConfig({
-  base: './',
-  resolve: {
-    alias: [
-      { find: /@\//, replacement: pathResolve('src') + '/' },
-      { find: /#\//, replacement: pathResolve('types') + '/' },
-    ],
+const buildlib = {
+  lib: {
+    entry: pathResolve('src/components/toc/index.js'),
+    name: 'vue3outline',
+    fileName: (format) => `vue3outline.${format}.js`,
   },
-  plugins: [vue()],
-  build: {
-    lib: {
-      entry: pathResolve('src/components/toc/index.js'),
-      name: 'vue3outline',
-      fileName: (format) => `vue3outline.${format}.js`,
-    },
-    sourcemap: true,
-    rollupOptions: {
-      external: ['vue'],
-      output: { globals: { vue: 'Vue' } },
-    },
+  sourcemap: true,
+  rollupOptions: {
+    external: ['vue'],
+    output: { globals: { vue: 'Vue' } },
   },
+};
+const builddemo = {};
+
+export default defineConfig(({ command, mode }) => {
+  console.log('[vite.config.ts] command %s mode %s', command, mode);
+  return {
+    base: './',
+    resolve: {
+      alias: [
+        { find: /@\//, replacement: pathResolve('src') + '/' },
+        { find: /#\//, replacement: pathResolve('types') + '/' },
+      ],
+    },
+    plugins: [vue()],
+    build: mode === 'demo' ? builddemo : buildlib,
+  };
 });
